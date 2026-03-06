a6d9a3a78d670c54f723284f3f896ca8e656b44f
paeru12 <herupratama8525@gmail.com>
2026-03-06 13:27:18 +0700

    upd reg

--- src/app/(dashboard)/regions/page.jsx
+++ src/app/(dashboard)/regions/page.jsx
@@ -1,12 +1,12 @@
-"use client";
+// "use client";
 
-import RequireRole from "@/components/common/RequireRole";
-import RegionManager from "@/components/features/eventadmin/RegionManager";
+// import RequireRole from "@/components/common/RequireRole";
+// import RegionManager from "@/components/features/eventadmin/RegionManager";
 
-export default function RegionsPage() {
-    return (
-        <RequireRole allowed={["EVENT_ADMIN", "SUPERADMIN"]}>
-            <RegionManager />
-        </RequireRole>
-    );
-}
+// export default function RegionsPage() {
+//     return (
+//         <RequireRole allowed={["EVENT_ADMIN", "SUPERADMIN"]}>
+//             <RegionManager />
+//         </RequireRole>
+//     );
+// }
